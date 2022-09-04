93b86cea79b1e3b1affdc6cdb907cc8329bf71a0
alexNerazim <99650672+alexNerazim@users.noreply.github.com>
2022-09-04 21:07:49 +0200

    Add Inbox contract

---
+++ inbox/contracts/Inbox.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.16;
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory initialMessage){
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+}
