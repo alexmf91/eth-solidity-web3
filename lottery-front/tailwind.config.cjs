952185dbd81792ee576445fad1133f98af16226e
alexNerazim <99650672+alexNerazim@users.noreply.github.com>
2022-10-09 19:55:07 +0200

    Add lottery-front

---
+++ lottery-front/tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,tsx}",
+    "./src/components/*.tsx",
+    "./src/views/**/*.tsx",
+    "./index.html"
+  ],
+  theme: {},
+  plugins: []
+};
