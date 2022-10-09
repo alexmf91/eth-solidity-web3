239086e719470f5756daef864a005a1cf536b97b
alexNerazim <99650672+alexNerazim@users.noreply.github.com>
2022-10-09 19:01:41 +0200

    Add comment

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const ganache = require("ganache");
+const ganache = require("ganache"); // ganache is used to create a local test network and accounts
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 const { abi, bytecode } = require("../compile");
