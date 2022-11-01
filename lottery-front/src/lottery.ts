237c6a3f23ad7f558f8cbfc25914f83d15d6fde1
alexNerazim <alex@nerazim.com>
2022-11-01 22:31:16 +0100

    Add lottery abi

---
+++ lottery-front/src/lottery.ts
@@ -0,0 +1,60 @@
+import { web3 } from './libs'
+
+const address = '0x13F9867556352aF8470779Ccf1aa652DCF612b1c'
+
+const abi = [
+  {
+    inputs: [],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+    signature: 'constructor'
+  },
+  {
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+    payable: true,
+    signature: '0xe97dcb62'
+  },
+  {
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [
+      { internalType: 'address payable[]', name: '', type: 'address[]' }
+    ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    signature: '0x8b5b9ccc'
+  },
+  {
+    inputs: [],
+    name: 'manager',
+    outputs: [{ internalType: 'address payable', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    signature: '0x481c6a75'
+  },
+  {
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    signature: '0x5d495aea'
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ internalType: 'address payable', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    signature: '0xf71d96cb'
+  }
+]
+
+export default new web3.eth.Contract(abi, address)
