b48a1e6320ac5ffbacbf9025957da8042c5e6bff
alexNerazim <alex@nerazim.com>
2022-11-01 22:31:45 +0100

    Change app alias

--- lottery-front/vite.config.ts
+++ lottery-front/vite.config.ts
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8000
+    port: 3000
   },
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }]
+    alias: [{ find: "@app", replacement: "/src" }]
   }
 });
