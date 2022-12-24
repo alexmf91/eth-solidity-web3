2ce08150bab6ba27f46cb98be06218f5d1f505b7
alexNerazim <alex@nerazim.com>
2022-12-24 18:48:34 +0100

    Add new methods: - Get Sumarry - Get request count - Get approvers count

--- crowd-coin/ethereum/contracts/Campaign.sol
+++ crowd-coin/ethereum/contracts/Campaign.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.7;
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
-    function createCampaign(uint minimum) public {
+    function createCampaign(uint256 minimum) public {
         address newCampaign = address(new Campaign(minimum, msg.sender));
         deployedCampaigns.push(newCampaign);
     }
 
-    function getDeployedCampaigns() public view returns (address[] memory){
+    function getDeployedCampaigns() public view returns (address[] memory) {
         return deployedCampaigns;
     }
 }
@@ -18,18 +18,19 @@ contract CampaignFactory {
 contract Campaign {
     struct Request {
         string description;
-        uint value;
+        uint256 value;
         address payable recipient;
         bool complete;
-        uint approvalCount;
+        uint256 approvalCount;
         mapping(address => bool) approvals;
     }
 
-    uint _numRequests;
-    uint public minimumContribution;
-    uint public approversCount;
+    uint256 _numRequests;
+    uint256 public minimumContribution;
+    uint256 public approversCount;
+    uint256 public requestsCount;
     address payable public manager;
-    mapping (uint => Request) public requests;
+    mapping(uint256 => Request) public requests;
     mapping(address => bool) public approvers;
 
     modifier restricted() {
@@ -37,7 +38,7 @@ contract Campaign {
         _;
     }
 
-    constructor(uint minimum, address creator) {
+    constructor(uint256 minimum, address creator) {
         manager = payable(creator);
         minimumContribution = minimum;
     }
@@ -48,17 +49,23 @@ contract Campaign {
         approversCount++;
     }
 
-    function createRequest(string memory description, uint value, address payable recipient) public restricted {
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public restricted {
         Request storage newRequest = requests[_numRequests++];
 
-        newRequest.description= description;
-        newRequest.value= value;
-        newRequest.recipient= recipient;
-        newRequest.complete= false;
-        newRequest.approvalCount= 0;
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+
+        requestsCount++;
     }
 
-    function approveRequest(uint index) public {
+    function approveRequest(uint256 index) public {
         Request storage request = requests[index];
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
@@ -67,12 +74,40 @@ contract Campaign {
         request.approvalCount++;
     }
 
-    function finalizeRequest(uint index) public restricted {
+    function finalizeRequest(uint256 index) public restricted {
         Request storage request = requests[index];
         require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
 
         request.recipient.transfer(request.value);
         request.complete = true;
+    }
+
+    function getSummary()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            address
+        )
+    {
+        return (
+            minimumContribution,
+            address(this).balance,
+            requestsCount,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint256) {
+        return requestsCount;
+    }
+
+    function getApproversCount() public view returns (uint256) {
+        return approversCount;
     }
 }
