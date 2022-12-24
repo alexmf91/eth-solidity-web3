0920d62810c72a0c066ce21d9b275a109da921eb
alexNerazim <alex@nerazim.com>
2022-12-24 19:19:38 +0100

    Ingore ts issue provisionally

--- crowd-coin/ethereum/campaign.ts
+++ crowd-coin/ethereum/campaign.ts
@@ -3,5 +3,6 @@ import { web3 } from '@crowd-coin/libs'
 import Campaign from './build/Campaign.json'
 
 export default function getCampaignInstance(address: string) {
+  //@ts-ignore
   return new web3.eth.Contract(Campaign.abi, address)
 }
--- crowd-coin/ethereum/factory.ts
+++ crowd-coin/ethereum/factory.ts
@@ -3,6 +3,7 @@ import { web3 } from '@crowd-coin/libs'
 import CampaignFactory from './build/CampaignFactory.json'
 
 const instance = new web3.eth.Contract(
+  //@ts-ignore
   CampaignFactory.abi,
   process.env.CONTRACT_ADDRESS || process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
 )
